67daf88c7f8e5464d302086689aabc23a32b6eac
Abdulhalim Muhammad Yaro <123118801+Abdulhalim7177@users.noreply.github.com>
2025-11-09 20:34:07 +0100

    initial setup

--- supabase/migrations/20251109050003__add_cluster_id_to_user_profiles.sql
+++
@@ -1,5 +0,0 @@
--- Add cluster_id column to user_profiles after clusters table is created
-ALTER TABLE user_profiles ADD COLUMN cluster_id UUID REFERENCES clusters(id);
-
--- Create index for the new column
-CREATE INDEX idx_user_profiles_cluster_id ON user_profiles(cluster_id);
